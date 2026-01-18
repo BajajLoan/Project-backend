472f27bc74c40a2e40a94cba4b6dab278690d778
BajajLoan <serviceinvestor.bajaj@gmail.com>
2026-01-19 02:27:19 +0530

    Update authRoute.js

--- router/authRoute.js
+++ router/authRoute.js
@@ -6,7 +6,7 @@ require("dotenv").config();
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  service: "email",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
